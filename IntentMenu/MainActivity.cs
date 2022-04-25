de349820ae32993cae8829bfb5efb4ad3a136c6d
Roberto <nicaragua_6002@yahoo.es>
2022-04-25 09:26:38 -0600

    Cn Intent

--- IntentMenu/MainActivity.cs
+++ IntentMenu/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace IntentMenu
@@ -8,13 +10,23 @@ namespace IntentMenu
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button btn1;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            btn1 = FindViewById<Button>(Resource.Id.button1);
+            btn1.Click += Btn1_Click;
         }
+
+        private void Btn1_Click(object sender, System.EventArgs e)
+        {
+            Intent i = new Intent(this, typeof(ActivitySegundo));
+            StartActivity(i);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
